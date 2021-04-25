7a663448f5f55d30405298d09f32f80671081f6a
AndrzejGretkowski <andrzejgretkowski5@gmail.com>
2021-04-25 23:20:01 +0200

    Add lab3 solutions

--- lab3/zadania_eigen.py
+++ lab3/zadania_eigen.py
@@ -43,21 +43,74 @@ def visualize_vectors(vectors, color="green"):
 
 def plot_eigenvectors(A):
     """Plots all eigenvectors of the given 2x2 matrix A."""
-    # TODO: Zad. 4.1. Oblicz wektory własne A. Możesz wykorzystać funkcję np.linalg.eig
-    eigvec = []
+    # TODO: Zad. 4.1. Oblicz wektory własne A. Możesz wykorzystać funkcję np.linalg.eig#
+    _, eigvec = np.linalg.eig(A)
     # TODO: Zad. 4.1. Upewnij się poprzez analizę wykresów, że rysowane są poprawne wektory własne (łatwo tu o pomyłkę).
     visualize_vectors(eigvec)
 
 
 def EVD_decomposition(A):
     # TODO: Zad. 4.2. Uzupełnij funkcję tak by obliczała rozkład EVD zgodnie z zadaniem.
-    pass
+    eigval, eigvec = np.linalg.eig(A)
+    K = eigvec
+    K_inv = np.linalg.inv(K)
+    L = np.diag(eigval)
+    print(K_inv, L, K)
+    # print(K_inv.dot(L).dot(K), A)
+    # assert np.array_equal(K_inv.dot(L).dot(K), A)
 
 
 def plot_attractors(A, vectors):
     # TODO: Zad. 4.3. Uzupełnij funkcję tak by generowała wykres z atraktorami.
-    pass
+    color_gen = (c for c in plt.get_cmap('tab10').colors)
+    eig_mapping = []
+    eigval, eigvec = np.linalg.eig(A)
 
+    colors = []
+    for vector in vectors:
+        vector = normalize(vector)
+        vec_attractor = find_attractor(A, vector)
+        if np.allclose(vec_attractor, vector):
+            colors.append((0, 0, 0))
+        else:
+            found = False
+            for (val, color) in eig_mapping:
+                if np.allclose(vec_attractor, val, rtol=0.01):
+                    colors.append(color)
+                    found = True
+            if not found:
+                c = next(color_gen)
+                eig_mapping.append((vec_attractor, c))
+                colors.append(c)
+
+    for vector, color in zip(vectors, colors):
+       plot_vector(normalize(vector), color, 0.004, 3)
+
+    for vector, color in eig_mapping:
+        plot_vector(normalize(vector), color, 0.004, 8)
+
+    plt.xlim([-2, 2])
+    plt.ylim([-2, 2])
+    plt.margins(0.05)
+    plt.grid()
+
+    plt.show()
+
+def find_attractor(A, vector):
+    new_v = np.copy(vector)
+    for _ in range(1000):
+        new_v = normalize(A @ new_v)
+    return new_v
+
+def normalize(v):
+    norm = np.linalg.norm(v)
+    if norm == 0:
+       return v
+    return v / norm
+
+def plot_vector(vector, color, width, headwidth):
+    q = plt.quiver(0.0, 0.0, vector[0], vector[1], width=width, color=color, headwidth=headwidth, scale_units='xy', angles='xy', scale=1,
+               zorder=4)
 
 def show_eigen_info(A, vectors):
     EVD_decomposition(A)
